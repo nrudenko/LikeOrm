d0b8502e7cda12720bdc08be6f46341f3624435a
Aleksey Malevaniy <a.malevaniy@gmail.com>
2014-03-31 11:46:10 +0300

    FIX :: column convertation condition

--- LikeOrmLib/src/main/java/com/github/nrudenko/orm/ReflectionUtils.java
+++ LikeOrmLib/src/main/java/com/github/nrudenko/orm/ReflectionUtils.java
@@ -11,7 +11,6 @@ import com.github.nrudenko.orm.commons.FieldType;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 
 public class ReflectionUtils {
 
@@ -68,7 +67,7 @@ public class ReflectionUtils {
             sqlRep = TextUtils.concat(sqlRep, " ", customAdditional).toString();
         }
         Column column = null;
-        if (TextUtils.isEmpty(columnName) || TextUtils.isEmpty(sqlRep)) {
+        if (!TextUtils.isEmpty(columnName) || !TextUtils.isEmpty(sqlRep)) {
             column = new Column(columnName, sqlRep);
         }
         return column;
